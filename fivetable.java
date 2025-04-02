3ff57235b424c579d2c398c354d2245c101097ae
Bhavi1804 <bhavikachavan560@gmail.com>
2025-04-02 12:15:57 +0530

    Add files via upload

---
+++ fivetable.java
@@ -0,0 +1,13 @@
+package Assignment_1.Assignment3;
+
+
+class FiveTable {
+
+    public static void main(String[] args) {
+        int number = 5;
+        System.err.println("Table of Five Is :-");
+        for (int i = 1; i <= 10; i++) {
+            System.err.println((i * number) + "\s"); 
+        }
+    }
+}
---
+++ fivetimehellow.java
@@ -0,0 +1,12 @@
+package Assignment_1.Assignment3;
+
+ class FiveTimeHellow {
+
+    public static void main(String[] args) {
+        int number = 5;
+        for (int i = 1; i <= number; i++) {
+            System.err.println("Hellow");
+        }
+      
+    }
+} 
